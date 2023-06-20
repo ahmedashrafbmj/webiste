cccade7fe6920d37c04b4af592aba55b626ffe81
Mian Muhammad Nadeem <LENOVO@ahmedinternew.hnh.local>
2023-06-20 11:35:49 +0500

    ahmed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,20 @@ export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init();
   }, []);
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = 'https://embed.tawk.to/64914622cc26a871b023913a/1h3bnjudg';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.head.appendChild(script);
+  
+    return () => {
+      // Clean up the script when the component is unmounted
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
     <Suspense>
